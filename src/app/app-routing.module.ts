b284fe45840e91431eba8de8b4f0b9c36ba6b150
Sok205 <filip007lol@gmail.com>
2023-01-31 14:48:27 +0100

    Dodanie komponentu nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './comp/nav/nav.component';
 import { RegisterComponent } from './comp/register/register.component';
 
 const routes: Routes = [
-  {path: 'register', component: RegisterComponent }
+  {path: 'register', component: RegisterComponent },
+  {path: 'nav', component: NavComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './comp/register/register.component';
+import { NavComponent } from './comp/nav/nav.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
