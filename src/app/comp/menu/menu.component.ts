a744ce8cf27f6d87c9bad31b4ce0a06766d3ab35
Sok205 <filip007lol@gmail.com>
2023-02-07 14:40:48 +0100

    Menu tweaks

--- src/app/comp/menu/menu.component.ts
+++ src/app/comp/menu/menu.component.ts
@@ -7,17 +7,11 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
   pizza =[
-    {id:1, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:2, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:3, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:4, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:5, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:6, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:7, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:8, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:9, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:10, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:11, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
-    {id:12, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg' },
+    {id:1, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg',toppings:"Tomato sauce, mozzarella, basil " },
+    {id:2, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg',toppings:"Tomato sauce, mozzarella, basil " },
+    {id:3, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg',toppings:"Tomato sauce, mozzarella, basil " },
+    {id:4, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg',toppings:"Tomato sauce, mozzarella, basil " },
+    {id:5, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg',toppings:"Tomato sauce, mozzarella, basil " },
+    {id:6, name: "Margherita", price: 13.55 , vegetarian:"Yes", image:'/assets/banner.jpg',toppings:"Tomato sauce, mozzarella, basil " },
   ]
 }
